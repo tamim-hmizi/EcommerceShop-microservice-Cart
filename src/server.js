9842a6ca695117256f034bb73e006819d6b1c38e
Tamim Hmizi <hmizitamim@hotmail.com>
2025-06-13 05:57:42 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use("/api/cart", cartRoutes);
+app.use("/api/carts", cartRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
